729a5e572a279a955a932b79002a061174f085f3
hongddoni <hongseunghyeok@Hongui-MacBookPro.local>
2024-09-06 09:43:09 +0900

    로컬 스토리지연결한 채팅방개발

--- packages/themind/src/App.tsx
+++ packages/themind/src/App.tsx
@@ -1,12 +1,14 @@
 import {Card} from "./components/card/Card.tsx";
+import {ChatButton} from "./components/chat/ChatButton.tsx";
 
 function App() {
 
-  return (
-    <>
-      <Card/>
-    </>
-  )
+    return (
+        <div style={{position: "relative"}}>
+            <Card/>
+            <ChatButton/>
+        </div>
+    )
 }
 
 export default App
